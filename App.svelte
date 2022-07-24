400daa8af2444f41d41b995965a5057267cae2c7
Hunter Bourgeois <79214781+bahunter105@users.noreply.github.com>
2022-07-24 19:40:36 +0200

    Added 2 different pages

--- App.svelte
+++ App.svelte
@@ -1,31 +1,42 @@
 <script>
-	import { MaterialApp, AppBar, Divider, Button, Icon } from "svelte-materialify";
+						import { MaterialApp, AppBar, Divider, Button, Icon } from "svelte-materialify";
+						import Page1 from "./views/Page1.svelte";
+						import Page2 from "./views/Page2.svelte";
+						import { mdiMenu } from "@mdi/js";
+            let theme = "light"
+            let page = Page1
+            
+						function toggleTheme() {
+						  if (theme === "light") theme = "dark";
+						  else theme = "light";
+						  // console.log("Hi");
+						  // theme = "dark";
+						}
 
-	let theme = "light";
-
-	function toggleTheme() {
-	  if (theme === "light") theme = "dark";
-	  else theme = "light";
-	}
-	import { mdiMenu } from "@mdi/js";
+						function handleClick() {
+						  if (page === Page1) page = Page2;
+						  else page = Page1;
+						  // console.log("What?");
+						  // page = Page1;
+						}
 </script>
 
 <style>
-  main {
-    font-family: sans-serif;
-    text-align: center;
-    display: flex;
-    height: 100vh;
-    width: 100vw;
-  }
+  /* main {
+                font-family: sans-serif;
+                text-align: center;
+                display: flex;
+                height: 100vh;
+                width: 100vw;
+              }
 
-  .card-background {
-    background-color: greenyellow;
-    border-radius: 5%;
-    height: 80vh;
-    width: 90vw;
-    margin: auto auto auto auto;
-  }
+              .card-background {
+                background-color: greenyellow;
+                border-radius: 5%;
+                height: 80vh;
+                width: 90vw;
+                margin: auto auto auto auto;
+              } */
 </style>
 
 <MaterialApp {theme}>
@@ -35,24 +46,11 @@
         <Icon  path={mdiMenu}/>
       </Button>
     </div>
-    <span slot="title"> Click The Menu </span>
+    <span slot="title"> {page} </span>
   </AppBar>
 	<br />
-  <div class="card-background">
-      
-
-    <h2 class="mb-4">
-      Svelte Materialify
-    </h2>
-    <Divider />
-    <br>
-    <div class="text-center">
-      <a href="/Page2.svelte">
-        <Button class="primary-color">
-          Page 2
-        </Button>
-      </a>
-      <Button on:click={toggleTheme}>Toggle Theme</Button>
-    </div>
-  </div>
+  <svelte:component this={page}
+    {toggleTheme}
+    {handleClick}
+  />
 </MaterialApp>
--- views/Page1.svelte
+++ views/Page1.svelte
@@ -2,7 +2,7 @@
  import { MaterialApp, AppBar, Divider, Button, Icon } from "svelte-materialify";
  //  export let theme;
  //  export let page;
-
+ export let title = "Page 1";
  export let toggleTheme;
  export let handleClick;
 </script>
@@ -18,16 +18,14 @@
 </style>
 
   <div class="card-background">
-      
-
     <h2 class="mb-4">
-      Page 2
+      Page 1
     </h2>
     <Divider />
     <br>
     <div class="text-center">
         <Button class="primary-color" on:click = {handleClick}>
-          Go to Page 1
+          Go to Page 2
         </Button>
         <Button on:click={toggleTheme}>Toggle Theme</Button>
     </div>
